198eedfbeb7a490a6848d6bcc11034de608d8967
Humberto <betoveenmorales@gmail.com>
2023-10-03 08:18:35 -0600

    Loading

--- ExpoMaterialTopTabs/src/screens/GroupsScreen.jsx
+++ ExpoMaterialTopTabs/src/screens/GroupsScreen.jsx
@@ -1,14 +1,30 @@
 import React from "react";
-import { Image } from "react-native";
+import { ActivityIndicator, Image } from "react-native";
 import { View, Text, StyleSheet } from "react-native";
 import { Girl } from "../../assets";
+import { useEffect, useState } from "react";
 
 
 export const GroupsScreen = () =>{
+
+    const [isloading, setLoading] = useState(false)
+
+    const delay = (ms) => new Promise((res) => setTimeout(res, ms))
+
+    useEffect(() => {
+        const cargar = async () =>{
+            setLoading(true)
+            await delay(4000)
+            setLoading(false)
+        }
+
+        cargar()
+    }, [])
+
     return(
         <View style={styles.container}>
             <Text style={{color: 'white' , fontSize: 30}} >Your Groups</Text>
-            <Image source={Girl} style={styles.imageStyles}/>
+            {isloading?(<ActivityIndicator size='large' />):<Image source={Girl} style={styles.imageStyles}/>}
         </View>
     )
 }
